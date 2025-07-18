8a91e759ea6dfe76ff13a9428987bb2e303c8f10
zbh <zbh@shushi.pro>
2025-07-18 14:33:13 +0800

    fix: remove invalid code

--- pamirs-framework/pamirs-connectors-data/src/main/java/pro/shushi/pamirs/framework/connectors/data/datasource/service/DefaultDsApi.java
+++ pamirs-framework/pamirs-connectors-data/src/main/java/pro/shushi/pamirs/framework/connectors/data/datasource/service/DefaultDsApi.java
@@ -67,7 +67,6 @@ public class DefaultDsApi implements DsApi {
         return Optional.ofNullable(fetchModuleDsMap())
                 .map(v -> v.get(ModuleConstants.MODULE_BASE))
                 .map(v -> DataPrefixManager.dsPrefix(ModuleConstants.MODULE_BASE, model, v))
-//                .orElse(defaultDsKey());
                 .orElse(pamirsFrameworkSystemConfiguration.getOriginSystemDsKey());
     }
 
