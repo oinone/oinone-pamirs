a72db6998bae0bb2faa585e906306172e9934291
gesi <linxuanhui@shushi.pro>
2025-08-14 17:53:20 +0800

    feat: mcp工具添加兼容集成接口的queryParam模式

--- pamirs-core/pamirs-eip2/pamirs-eip2-designer/src/main/java/pro/shushi/pamirs/eip/designer/util/EipParamsHelper.java
+++ pamirs-core/pamirs-eip2/pamirs-eip2-designer/src/main/java/pro/shushi/pamirs/eip/designer/util/EipParamsHelper.java
@@ -6,12 +6,11 @@ import pro.shushi.pamirs.eip.api.IEipContext;
 import pro.shushi.pamirs.eip.api.enmu.ContextTypeEnum;
 import pro.shushi.pamirs.eip.api.model.EipConvertParam;
 import pro.shushi.pamirs.eip.api.pamirs.DefaultOpenFunctionConverterFunction;
-import pro.shushi.pamirs.eip.designer.model.open.EipOpenReqBodyParam;
-import pro.shushi.pamirs.eip.designer.model.open.EipOpenReqHeaderParam;
-import pro.shushi.pamirs.eip.designer.model.open.EipOpenReqQueryParam;
-import pro.shushi.pamirs.eip.designer.model.open.EipOpenRespParam;
+import pro.shushi.pamirs.eip.designer.model.conn.*;
+import pro.shushi.pamirs.eip.designer.model.open.*;
 import pro.shushi.pamirs.meta.common.constants.CharacterConstants;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -103,6 +102,126 @@ public class EipParamsHelper {
                 buildRespParams(responseConvertParams, param.getChildren(), key);
             }
         }
+    }
+
+    public static List<EipOpenReqQueryParam> buildQueryParams(List<EipReqQueryParam> queryParams) {
+        List<EipOpenReqQueryParam> openApiQueryParams = new ArrayList<>(queryParams.size());
+        for (EipReqQueryParam queryParam : queryParams) {
+            EipOpenReqQueryParam openApiQueryParam = new EipOpenReqQueryParam();
+            openApiQueryParam.setKey(queryParam.getKey());
+            openApiQueryParam.setRequired(queryParam.getRequired());
+            openApiQueryParam.setDefaultValue(queryParam.getDefaultValue());
+            openApiQueryParam.setDesc(queryParam.getDesc());
+            openApiQueryParam.setValueExpr(queryParam.getKey());
+            openApiQueryParam.setParamType(queryParam.getParamType());
+            openApiQueryParams.add(openApiQueryParam);
+        }
+        return openApiQueryParams;
+    }
+
+    public static List<EipOpenReqPathParam> buildPathParams(List<EipReqPathParam> pathParams) {
+        List<EipOpenReqPathParam> openApiPathParams = new ArrayList<>(pathParams.size());
+        for (EipReqPathParam pathParam : pathParams) {
+            EipOpenReqPathParam openApiPathParam = new EipOpenReqPathParam();
+            openApiPathParam.setKey(pathParam.getKey());
+            openApiPathParam.setRequired(pathParam.getRequired());
+            openApiPathParam.setDefaultValue(pathParam.getDefaultValue());
+            openApiPathParam.setDesc(pathParam.getDesc());
+            openApiPathParam.setParamType(pathParam.getParamType());
+            openApiPathParams.add(openApiPathParam);
+        }
+        return openApiPathParams;
+    }
+
+    public static List<EipOpenReqHeaderParam> buildHeaderParams(List<EipReqHeaderParam> headerParams) {
+        List<EipOpenReqHeaderParam> openApiHeaderParams = new ArrayList<>(headerParams.size());
+        for (EipReqHeaderParam headerParam : headerParams) {
+            EipOpenReqHeaderParam openApiHeaderParam = new EipOpenReqHeaderParam();
+            openApiHeaderParam.setKey(headerParam.getKey());
+            openApiHeaderParam.setRequired(headerParam.getRequired());
+            openApiHeaderParam.setDefaultValue(headerParam.getDefaultValue());
+            openApiHeaderParam.setDesc(headerParam.getDesc());
+            openApiHeaderParam.setParamType(headerParam.getParamType());
+            openApiHeaderParams.add(openApiHeaderParam);
+        }
+        return openApiHeaderParams;
+    }
+
+    public static List<EipOpenReqBodyParam> buildBodyParams(List<EipReqBodyParam> bodyParams) {
+        return buildBodyParams(bodyParams, null);
+    }
+
+    public static List<EipOpenReqBodyParam> buildBodyParams(List<EipReqBodyParam> bodyParams, String valueExprPrefix) {
+        List<EipOpenReqBodyParam> openApiBodyParams = new ArrayList<>(bodyParams.size());
+        for (EipReqBodyParam bodyParam : bodyParams) {
+            Boolean isMulti = Optional.ofNullable(bodyParam.getIsMulti()).orElse(Boolean.FALSE);
+            String valueExpr = bodyParam.getKey();
+            if (StringUtils.isNotBlank(valueExprPrefix)) {
+                valueExpr = valueExprPrefix + CharacterConstants.SEPARATOR_DOT + valueExpr;
+            }
+            if (isMulti) {
+                valueExpr = valueExpr + IEipContext.DEFAULT_LIST_FLAG_KEY;
+            }
+            EipOpenReqBodyParam openApiBodyParam = new EipOpenReqBodyParam();
+            openApiBodyParam.setKey(bodyParam.getKey());
+            openApiBodyParam.setRequired(bodyParam.getRequired());
+            openApiBodyParam.setDefaultValue(bodyParam.getDefaultValue());
+            openApiBodyParam.setDesc(bodyParam.getDesc());
+            openApiBodyParam.setParamType(bodyParam.getParamType());
+            openApiBodyParam.setSize(bodyParam.getSize());
+            openApiBodyParam.setIsMulti(isMulti);
+            openApiBodyParam.setValueExpr(valueExpr);
+            List<EipReqBodyParam> children = bodyParam.getChildren();
+            if (CollectionUtils.isNotEmpty(children)) {
+                openApiBodyParam.setChildren(buildBodyParams(children, valueExpr));
+            }
+            openApiBodyParams.add(openApiBodyParam);
+        }
+        return openApiBodyParams;
+    }
+
+    public static List<EipOpenRespParam> buildResponseParams(List<EipApiResponseParam> responseParams) {
+        return buildResponseParams(responseParams, null);
+    }
+
+    public static List<EipOpenRespParam> buildResponseParams(List<EipApiResponseParam> responseParams, String valueExprPrefix) {
+        List<EipOpenRespParam> openApiResponseParams = new ArrayList<>(responseParams.size());
+        for (EipApiResponseParam responseParam : responseParams) {
+            String valueExpr = responseParam.getKey();
+            if (StringUtils.isNotBlank(valueExprPrefix)) {
+                valueExpr = valueExprPrefix + CharacterConstants.SEPARATOR_DOT + valueExpr;
+            }
+            EipOpenRespParam openApiResponseParam = new EipOpenRespParam();
+            openApiResponseParam.setKey(responseParam.getKey());
+            openApiResponseParam.setParamType(responseParam.getParamType());
+            openApiResponseParam.setIsMulti(responseParam.getIsMulti());
+            openApiResponseParam.setValueExpr(valueExpr);
+            openApiResponseParam.setDesc(responseParam.getDesc());
+            List<EipApiResponseParam> children = responseParam.getChildren();
+            if (CollectionUtils.isNotEmpty(children)) {
+                openApiResponseParam.setChildren(buildResponseParams(children, valueExpr));
+            }
+            openApiResponseParams.add(openApiResponseParam);
+        }
+        return openApiResponseParams;
+    }
+
+    public static List<EipReqBodyParam> caseQueryParams2BodyParams(List<EipReqQueryParam> queryParams) {
+        if (CollectionUtils.isEmpty(queryParams)) {
+            return new ArrayList<>();
+        }
+        List<EipReqBodyParam> openApiBodyParams = new ArrayList<>(queryParams.size());
+        for (EipReqQueryParam queryParam : queryParams) {
+            EipReqBodyParam openApiBodyParam = new EipReqBodyParam();
+            openApiBodyParam.setKey(queryParam.getKey());
+            openApiBodyParam.setRequired(queryParam.getRequired());
+            openApiBodyParam.setDefaultValue(queryParam.getDefaultValue());
+            openApiBodyParam.setDesc(queryParam.getDesc());
+            openApiBodyParam.setValueExpr(queryParam.getKey());
+            openApiBodyParam.setParamType(queryParam.getParamType());
+            openApiBodyParams.add(openApiBodyParam);
+        }
+        return openApiBodyParams;
     }
 
 }
