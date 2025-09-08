67dfe4312fbd0715cb24ffa2b8fc55c925e71d03
zbh <zbh@shushi.pro>
2025-09-08 15:04:51 +0800

    fix: default environment checker

--- pamirs-boot/pamirs-boot-standard/src/main/java/pro/shushi/pamirs/boot/standard/checker/environment/AbstractPlatformEnvironmentChecker.java
+++ pamirs-boot/pamirs-boot-standard/src/main/java/pro/shushi/pamirs/boot/standard/checker/environment/AbstractPlatformEnvironmentChecker.java
@@ -12,6 +12,7 @@ import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertySource;
 import pro.shushi.pamirs.boot.standard.checker.PlatformEnvironmentChecker;
 import pro.shushi.pamirs.boot.standard.checker.constants.EnvironmentCheckConstants;
+import pro.shushi.pamirs.boot.standard.checker.helper.DefaultChecker;
 import pro.shushi.pamirs.boot.standard.entity.EnvironmentCheckContext;
 import pro.shushi.pamirs.boot.standard.entity.EnvironmentCheckResult;
 import pro.shushi.pamirs.boot.standard.entity.EnvironmentKey;
@@ -147,7 +148,7 @@ public abstract class AbstractPlatformEnvironmentChecker implements PlatformEnvi
         if (checker != null) {
             return checker.check(context, currentEnvironment, historyEnvironment);
         }
-        return defaultPropertyCompare(context, currentEnvironment, historyEnvironment);
+        return DefaultChecker.INSTANCE.check(context, currentEnvironment, historyEnvironment);
     }
 
     protected boolean createNewEnvironment(EnvironmentCheckContext context, PlatformEnvironment currentEnvironment) {
@@ -185,53 +186,6 @@ public abstract class AbstractPlatformEnvironmentChecker implements PlatformEnvi
             addDelete(context, historyEnvironment);
         }
         return true;
-    }
-
-    protected PlatformEnvironment defaultPropertyCompare(EnvironmentCheckContext context, PlatformEnvironment currentEnvironment, PlatformEnvironment historyEnvironment) {
-        if (context.isImmutableEnvironment()) {
-            addContext(context, currentEnvironment, historyEnvironment);
-        } else {
-            addUpdate(context, currentEnvironment);
-        }
-        return currentEnvironment;
-    }
-
-    protected void addContext(EnvironmentCheckContext context, PlatformEnvironment currentEnvironment, PlatformEnvironment historyEnvironment) {
-        String oldValue = historyEnvironment.getValue();
-        String newValue = currentEnvironment.getValue();
-        EnvironmentKey key = context.getKey(currentEnvironment);
-        switch (key.getLevel()) {
-            case IMMUTABLE:
-                if (newValue == null || !newValue.equals(oldValue)) {
-                    // FIXME: zbh 20250908 兼容旧版环境检查未处理环境变量的问题
-                    if (oldValue != null && oldValue.startsWith("${") && oldValue.endsWith("}")) {
-                        break;
-                    }
-                    addError(context, currentEnvironment, getErrorMessage(key.getMessage(), EnvironmentCheckConstants.IMMUTABLE_TIP + oldValue));
-                }
-                break;
-            case ADD:
-            case ADD_OR_DELETE:
-                if (oldValue != null && newValue != null && !oldValue.equals(newValue)) {
-                    // FIXME: zbh 20250908 兼容旧版环境检查未处理环境变量的问题
-                    if (oldValue.startsWith("${") && oldValue.endsWith("}")) {
-                        break;
-                    }
-                    addError(context, currentEnvironment, getErrorMessage(key.getMessage(), EnvironmentCheckConstants.IMMUTABLE_TIP + oldValue));
-                }
-                break;
-            case ERROR:
-                addError(context, currentEnvironment);
-                break;
-            case WARNING:
-                if (newValue == null || !newValue.equals(oldValue)) {
-                    addWarning(context, currentEnvironment);
-                }
-                break;
-            case DEPRECATED:
-                addDeprecated(context, currentEnvironment);
-                break;
-        }
     }
 
     protected String getErrorMessage(String message, String defaultMessage) {
