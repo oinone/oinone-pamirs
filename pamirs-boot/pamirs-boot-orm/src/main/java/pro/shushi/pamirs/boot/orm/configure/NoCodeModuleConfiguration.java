e56af4b38d0f5e2b16c1dd1440368dc8b0108925
zbh <zbh@shushi.pro>
2025-09-09 16:41:48 +0800

    feat: no code module resolve dependencies

--- pamirs-boot/pamirs-boot-orm/src/main/java/pro/shushi/pamirs/boot/orm/configure/NoCodeModuleConfiguration.java
+++ pamirs-boot/pamirs-boot-orm/src/main/java/pro/shushi/pamirs/boot/orm/configure/NoCodeModuleConfiguration.java
@@ -2,16 +2,24 @@ package pro.shushi.pamirs.boot.orm.configure;
 
 import pro.shushi.pamirs.meta.annotation.fun.Data;
 
-import java.io.Serializable;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Data
-public class NoCodeModuleConfiguration implements Serializable {
+public class NoCodeModuleConfiguration {
 
-    // 启动是否加载无代码模块. 默认安装. 页面新增无代码模块后,下次启动会被加载
-    private Boolean init = Boolean.TRUE;
+    /**
+     * 启动是否加载无代码模块. 默认安装. 页面新增无代码模块后,下次启动会被加载
+     */
+    private Boolean init = true;
 
-    // 启动无代码模块列表
-    private Set<String> modules = new HashSet<>();
+    /**
+     * 启动无代码模块列表
+     */
+    private Set<String> modules = new LinkedHashSet<>();
+
+    /**
+     * 指定启动无代码模块时是否自动解析无代码模块依赖加入到启动模块列表
+     */
+    private Boolean resolveDependency = true;
 }
