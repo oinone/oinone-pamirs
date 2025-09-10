83e67dcb8fc2e9b827dc239941c85a1a69646912
gesi <linxuanhui@shushi.pro>
2025-09-10 18:16:21 +0800

    feat: 添加快速填报模型设计

---
+++ pamirs-boot/pamirs-boot-ui/src/main/java/pro/shushi/pamirs/boot/web/service/QuickFillingServiceImpl.java
@@ -0,0 +1,18 @@
+package pro.shushi.pamirs.boot.web.service;
+
+import org.springframework.stereotype.Service;
+import pro.shushi.pamirs.boot.base.tmodel.QuickFilling;
+
+/**
+ *
+ * @author Gesi at 18:02 on 2025/9/10
+ */
+@Service
+public class QuickFillingServiceImpl implements QuickFillingService {
+
+    @Override
+    public QuickFilling loadData(QuickFilling quickFilling) {
+        return null;
+    }
+
+}
---
+++ pamirs-boot/pamirs-meta-ui-model/src/main/java/pro/shushi/pamirs/boot/base/tmodel/QuickFillingField.java
@@ -0,0 +1,22 @@
+package pro.shushi.pamirs.boot.base.tmodel;
+
+import pro.shushi.pamirs.meta.annotation.Field;
+import pro.shushi.pamirs.meta.annotation.Model;
+import pro.shushi.pamirs.meta.api.dto.config.ModelFieldConfig;
+import pro.shushi.pamirs.meta.base.TransientModel;
+
+/**
+ * @author Gesi at 17:15 on 2025/9/10
+ */
+@Model(displayName = "快速填报字段")
+@Model.model(QuickFillingField.MODEL_MODEL)
+public class QuickFillingField extends TransientModel {
+
+    public static final String MODEL_MODEL = "base.QuickFillingField";
+
+    @Field(displayName = "字段名")
+    private String field;
+
+    private ModelFieldConfig modelConfigField;
+
+}
