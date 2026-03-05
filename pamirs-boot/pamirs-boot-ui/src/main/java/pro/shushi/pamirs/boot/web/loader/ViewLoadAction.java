7d183a3ef718a579b8ef36c517f395421f1202f6
zbh <zbh@shushi.pro>
2026-03-05 16:17:28 +0800

    fix: remove invalid code

--- pamirs-boot/pamirs-boot-ui/src/main/java/pro/shushi/pamirs/boot/web/loader/ViewLoadAction.java
+++ pamirs-boot/pamirs-boot-ui/src/main/java/pro/shushi/pamirs/boot/web/loader/ViewLoadAction.java
@@ -1,23 +1,16 @@
 package pro.shushi.pamirs.boot.web.loader;
 
-import org.apache.commons.lang3.StringUtils;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import pro.shushi.pamirs.boot.base.model.View;
-import pro.shushi.pamirs.boot.base.ux.cache.api.ActionCacheApi;
-import pro.shushi.pamirs.boot.base.ux.cache.api.HighPriorityModelViewCacheApi;
-import pro.shushi.pamirs.boot.base.ux.cache.api.ViewCacheApi;
 import pro.shushi.pamirs.boot.web.service.ViewService;
 import pro.shushi.pamirs.meta.annotation.Fun;
 import pro.shushi.pamirs.meta.annotation.Function;
 import pro.shushi.pamirs.meta.annotation.sys.Base;
-import pro.shushi.pamirs.meta.api.session.PamirsSession;
 import pro.shushi.pamirs.meta.enmu.FunctionOpenEnum;
 import pro.shushi.pamirs.meta.enmu.FunctionTypeEnum;
-import pro.shushi.pamirs.meta.util.JsonUtils;
 
-import jakarta.annotation.Resource;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 视图管理器
@@ -48,42 +41,6 @@ public class ViewLoadAction {
     @Function.Advanced(type = FunctionTypeEnum.QUERY)
     public List<View> load(List<View> viewList) {
         return viewService.load(viewList);
-    }
-
-    @Function(summary = "辉导测试", openLevel = FunctionOpenEnum.API)
-    @Function.Advanced(type = FunctionTypeEnum.QUERY)
-    public View ddddddddd(View view) {
-        if (!"ZfKQHDNlj96hFYYbbU4m7di3hvaTMgeU".equals(view.getName())) {
-            return view;
-        }
-        String model = view.getModel();
-        Map<String, Object> param = JsonUtils.parseMap(view.getSummary());
-        if (null == param) {
-            view.setSummary(JsonUtils.toJSONString(PamirsSession.getContext().getModelConfig(model)));
-        } else {
-            String field = (String) param.get("field");
-            String fun = (String) param.get("fun");
-            String action = (String) param.get("action");
-            String viewName = (String) param.get("viewName");
-            String viewType = (String) param.get("viewType");
-            if (StringUtils.isNotBlank(field)) {
-                view.setSummary(JsonUtils.toJSONString(PamirsSession.getContext().getModelField(model, field)));
-            } else if (StringUtils.isNotBlank(fun)) {
-                view.setSummary(JsonUtils.toJSONString(PamirsSession.getContext().getFunctionAllowNull(model, fun)));
-            } else if (StringUtils.isNotBlank(action)) {
-                view.setSummary(JsonUtils.toJSONString(PamirsSession.getContext().getExtendCacheValue(ActionCacheApi.class,
-                        cache -> cache.get(model, action))));
-            } else if (StringUtils.isNotBlank(viewName)) {
-                view.setSummary(JsonUtils.toJSONString(PamirsSession.getContext().getExtendCacheValue(ViewCacheApi.class,
-                        cache -> cache.get(model, viewName))));
-            } else if (StringUtils.isNotBlank(viewType)) {
-                view.setSummary(JsonUtils.toJSONString(PamirsSession.getContext().getExtendCacheValue(HighPriorityModelViewCacheApi.class,
-                        cache -> cache.get(model, viewType))));
-            } else {
-                view.setSummary(JsonUtils.toJSONString(PamirsSession.getContext().getModelConfig(model)));
-            }
-        }
-        return view;
     }
 
 }
